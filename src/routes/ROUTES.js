10fe89d014c6e16b96a8caf2a5f93ddcb7488969
Ajxd9 <134958823+Ajxd9@users.noreply.github.com>
2024-02-06 19:16:14 +0200

    asd

--- src/routes/ROUTES.js
+++ src/routes/ROUTES.js
@@ -1,9 +1,10 @@
-const ROUTES={
-        HOME:"/",
-        REGISTER:"/register",
-        LOGIN:"/login",
-        EDITCARD:"/editcard",
-        CREATCAR:"/createcard",
-        SANDBOX:"/sandbox",
+const ROUTES = {
+  HOME: "/",
+  REGISTER: "/register",
+  LOGIN: "/login",
+  EDITCARD: "/editcard",
+  CREATCAR: "/createcard",
+  SANDBOX: "/sandbox",
+  NOTFOUND: "/404",
 };
 export default ROUTES;
--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -1,16 +1,18 @@
-import React from 'react'
-import { Route, Routes } from 'react-router-dom'
-import HomePage from "../pages/HomePage"
-import LoginPage from "../pages/LoginPage/SigninPage"
-import ROUTES from './ROUTES'
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import HomePage from "../pages/HomePage";
+import LoginPage from "../pages/LoginPage/SigninPage";
+import ROUTES from "./ROUTES";
+import NotFoundPage from "../pages/NotFoundPage/NotFoundPage";
 const Router = () => {
   return (
     <Routes>
-        <Route path={ROUTES.HOME} element={<HomePage/>}/>
-        <Route path={ROUTES.LOGIN} element={<LoginPage/>}/>
-        <Route path={ROUTES.REGISTER} element={<LoginPage/>}/>
+      <Route path={ROUTES.HOME} element={<HomePage />} />
+      <Route path={ROUTES.LOGIN} element={<LoginPage />} />
+      <Route path={ROUTES.REGISTER} element={<LoginPage />} />
+      <Route path={ROUTES.NOTFOUND} element={<NotFoundPage />} />
     </Routes>
-  )
-}
+  );
+};
 
-export default Router
+export default Router;

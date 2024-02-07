61d1b26f0673ee93b978bbd0c5252c74b18dcad8
Ajxd9 <134958823+Ajxd9@users.noreply.github.com>
2024-02-07 06:27:40 +0200

    more files

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,21 @@
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
-import { CssBaseline } from '@mui/material';
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+import { CssBaseline } from "@mui/material";
 
-import { useEffect, useState } from 'react';
-import { BrowserRouter } from 'react-router-dom';
-import LayoutPage from './layouts/LayoutPage';
-import NotFoundPage from './pages/NotFoundPage.css/NotFoundPage';
-import Router from './routes/Router';
-
+import { useEffect, useState } from "react";
+import { BrowserRouter } from "react-router-dom";
+import LayoutPage from "./layouts/LayoutPage";
+import Router from "./routes/Router";
 
 function App() {
-  
-
   return (
-    <div className='container'>
-    <CssBaseline/>
-    <LayoutPage>
-      <Router />
-    </LayoutPage>
+    <div className="container">
+      <CssBaseline />
+      <LayoutPage>
+        <Router />
+      </LayoutPage>
     </div>
   );
 }
